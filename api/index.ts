04b963fe4ff34704fa14f2ae11d5f6913c3d468c
Tisha <gargtisha10@gmail.com>
2025-07-02 21:02:41 +0530

    chnges

--- api/index.ts
+++ api/index.ts
@@ -26,41 +26,5 @@ app.use("/", (req, res) => {
   res.send("Welcome to track");
 });
 app.get("/api/user/me", userController.getUserById);
-  interface Subregion {
-    id: number;
-    name: string;
-  }
 
-  interface Region {
-    id: number;
-    name: string;
-    subregions: Subregion[];
-  }
-  app.get("/api/regions", (req: any, res: Response) => {
-    try {
-      const regions = regionsData.map((region) => ({
-        id: region.id,
-        name: region.name,
-      }));
-      res.json(regions);
-    } catch (error) {
-      res.status(500).json({ error: "Internal server error" });
-    }
-  });
-  app.get("/api/regions/:id", (req: any, res: Response) => {
-    try {
-      const id = req.params.id;
-      const region = regionsData.find(
-        (r) =>
-          r.id === parseInt(id) || r.name.toLowerCase() === id.toLowerCase()
-      );
-      if (!region) {
-        res.status(404).json({ error: "Region not found" });
-        return;
-      }
-      res.json(region);
-    } catch (error) {
-      res.status(500).json({ error: "Internal server error" });
-    }
-  });
 export default app;
